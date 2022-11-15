2425dbae609b7faa8c4249f0079818f7edabf5c1
Neda <nedachangizi@gmail.com>
2022-11-15 10:41:43 -0800

    popoulate shopping list

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
-function greet(userName, time = "Day") {
-  console.log(`Good ${time} ${userName}`);
-}
+// function greet(userName, time = "Day") {
+//   return(`Good ${time} ${userName}`);
+// }
 
-greet("Neda", "Night");
+// const result = greet("Neda", "Night");
+// console.log(result)
 
 // let greet = function (userName) {
 //   //   console.log("Hello " + userName);
@@ -12,3 +13,20 @@ greet("Neda", "Night");
 // let greet = (userName) => {
 //     console.log(`Hello ${userName}`);
 //   };
+
+const unorderedListElement = document.querySelector(".shopping");//DOM element
+function populateList (list)
+{
+    for (let i = 0 ; i <list.length ; i++)
+    {
+        console.log(list[i]); //for each of these items, create an li, append it to unorderedlistElement
+        const listItemElement = document.createElement("li");
+        listItemElement.textContent = list[i];
+        unorderedListElement.appendChild(listItemElement);
+
+    }
+}
+
+let shoppingList = ["cheese", "bread", "green pepper"];
+
+populateList(shoppingList);

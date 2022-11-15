fd26ca64d0eb2b0908df4f883cdb22d200b3f3d8
Neda <nedachangizi@gmail.com>
2022-11-15 09:45:00 -0800

    function practice

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// function greet(userName) {
+// //   console.log("Hello " + userName);
+// console.log(`Hello ${userName}`);
+
+// }
+
+// let greet = function (userName) {
+//   //   console.log("Hello " + userName);
+//   console.log(`Hello ${userName}`);
+// };
+
+let greet =  (userName) => {
+    //   console.log("Hello " + userName);
+    console.log(`Hello ${userName}`);
+  };
+
+greet("Neda");
+greet("Tin");

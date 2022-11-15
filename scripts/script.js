34316e7fc68328dee40ed655a1b2c560b43d313b
Neda <nedachangizi@gmail.com>
2022-11-15 09:54:13 -0800

    more than one param

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,14 @@
-// function greet(userName) {
-// //   console.log("Hello " + userName);
-// console.log(`Hello ${userName}`);
+function greet(userName, time = "Day") {
+  console.log(`Good ${time} ${userName}`);
+}
 
-// }
+greet("Neda", "Night");
 
 // let greet = function (userName) {
 //   //   console.log("Hello " + userName);
 //   console.log(`Hello ${userName}`);
 // };
 
-let greet =  (userName) => {
-    //   console.log("Hello " + userName);
-    console.log(`Hello ${userName}`);
-  };
-
-greet("Neda");
-greet("Tin");
+// let greet = (userName) => {
+//     console.log(`Hello ${userName}`);
+//   };
